247cbc90df19cbfcf5590782ea8a17f9dfa5aa46
Mike Romanovich <mike.romanovich@gmail.com>
2020-08-28 11:24:49 +0300

    game run untill EOF

--- src/main/java/com/mr/texasholdem/Main.java
+++ src/main/java/com/mr/texasholdem/Main.java
@@ -9,31 +9,48 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
-        BufferedReader reader =
-            new BufferedReader(new InputStreamReader(System.in));
-
-        String inputString = null;
-        try {
-            inputString = reader.readLine();
-        }
-        catch (IOException e) {
-            System.out.println("input error: ");
-            e.printStackTrace(System.out);
-        }
-        String[] tokens = inputString.split("\\s+");
-        if (tokens == null || tokens.length < 2){
-            System.out.println("wrong input data");
-            return;
-        }
-        CommunityCards communityCards = new CommunityCards(tokens[0]);
-        List<SevenCard> sevenCards = new ArrayList<>();
-        for (int a = 1; a < tokens.length; a++){
-            SevenCard sevenCard = new SevenCard(communityCards, tokens[a]);
-            sevenCards.add(sevenCard);
-        }
-        Collections.sort(sevenCards);
-        System.out.print(communityCards.toString());
-        System.out.print(sevenCards.stream().reduce("", (s, sc)->s.concat(" " + sc.getHoleCardCodes()), String::concat));
+  public static void main(String[] args) {
+    while (true) {
+      String[] tokens = getTokens();
+      if (tokens.length == 0)
+        return;
+      CommunityCards communityCards = new CommunityCards(tokens[0]);
+      List<SevenCard> sevenCards = new ArrayList<>();
+      for (int a = 1; a < tokens.length; a++) {
+        SevenCard sevenCard = new SevenCard(communityCards, tokens[a]);
+        sevenCards.add(sevenCard);
+      }
+      Collections.sort(sevenCards);
+      System.out.print(communityCards.toString());
+      System.out.print(sevenCards.stream().reduce("", (s, sc) -> s.concat(" " + sc.getHoleCardCodes()), String::concat));
+      System.out.println();
     }
+  }
+
+  private static String[] getTokens() {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    String inputString = null;
+    String[] tokens;
+    while (true) {
+      try {
+        inputString = reader.readLine();
+      }
+      catch (IOException e) {
+        System.out.println("input error: ");
+        e.printStackTrace(System.out);
+      }
+      if (inputString == null) {
+        return new String[] {};
+      }
+      tokens = inputString.split("\\s+");
+      if (tokens.length < 2) {
+        System.out.println("wrong input data");
+      }
+      else {
+        return tokens;
+      }
+    }
+
+  }
 }
