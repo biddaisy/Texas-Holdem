8ed204c9342c35cacca31e90cdf19971045c0a0d
Mike Romanovich <mike.romanovich@gmail.com>
2020-08-30 12:52:10 +0300

    FullHouseEvaluator fixed

--- src/main/java/com/mr/texasholdem/evaluator/FullHouseEvaluator.java
+++ src/main/java/com/mr/texasholdem/evaluator/FullHouseEvaluator.java
@@ -1,28 +1,27 @@
 package com.mr.texasholdem.evaluator;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.mr.texasholdem.card.Card;
 import com.mr.texasholdem.hand.FullHouse;
 import com.mr.texasholdem.hand.Hand;
 import com.mr.texasholdem.hand.Pair;
-import com.mr.texasholdem.hand.FourOfAKind;
+import com.mr.texasholdem.hand.ThreeOfAKind;
 
 public class FullHouseEvaluator extends AbstractHandEvaluator {
   @Override
   public Hand evaluate(Card[] cards) {
-    FourOfAKind FourOfAKind = new FourOfAKindEvaluator().findFourOfAKind(asList(cards));
-    if (FourOfAKind == null)
+    ThreeOfAKind threeOfAKind = new ThreeOfAKindEvaluator().findThreeOfAKind(asList(cards));
+    if (threeOfAKind == null)
       return null;
     List<Card> remainingCards = asList(cards);
-    remainingCards.remove(FourOfAKind.getCard1());
-    remainingCards.remove(FourOfAKind.getCard2());
-    remainingCards.remove(FourOfAKind.getCard3());
+    remainingCards.remove(threeOfAKind.getCard1());
+    remainingCards.remove(threeOfAKind.getCard2());
+    remainingCards.remove(threeOfAKind.getCard3());
     Pair pair = new PairEvaluator().findPair(remainingCards);
     if (pair == null)
       return null;
-    return new FullHouse(FourOfAKind, pair);
+    return new FullHouse(threeOfAKind, pair);
   }
 
   @Override
